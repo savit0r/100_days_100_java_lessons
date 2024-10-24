4e1b38d9d626c022e9b0d3d48a040ca6aac42f6d
Rohit <rohitsatre33@gmail.com>
2024-10-24 12:07:43 +0530

    assignment 3.3 added

---
+++ LESSON-3/Assignment_3_3.java
@@ -0,0 +1,13 @@
+// 24/oct/2024
+
+public class Assignment_3_3{
+    public static void main(String [] args){
+
+        //For loop 
+        System.out.println("First 10 natural numbers");
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i) ;
+        }
+    }
+}
